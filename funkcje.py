ca974730f3902e737a5e15942c0c83ff96ac4598
Jakub Nawłatyna <twblowek@gmail.com>
2022-06-26 17:52:46 +0200

    utworzenie okna menu_start, dodanie funkcji do czytania z drugiej tabeli, dodanie zdj potrzebnego do ikony okienka

--- funkcje.py
+++ funkcje.py
@@ -1,16 +1,27 @@
-
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from functools import partial
 from tkinter import *
 baza = declarative_base()
+# tworzenie modeli
 class Dane_do_logowania(baza):
     __tablename__ = 'dane_do_logowania'
     login = db.Column(db.String(100),primary_key=True)
     haslo = db.Column(db.String(100))
+class Informacje(baza):
+    __tablename__ = 'informacje'
+    login = db.Column(db.String(100),primary_key=True)
+    record = db.Column(db.Integer)
+    games_played = db.Column(db.Integer)
 
+
+#funkcje
 def czytaj_dane_do_logowania(session):
     for dane in session.query(Dane_do_logowania):
         print(dane.login, dane.haslo)
+    print()
+def czytaj_informacje(session):
+    for informacje in session.query(Informacje):
+        print(informacje.login, informacje.record, informacje.games_played)
     print()
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
-
 from functools import partial
 from tkinter import *
-
 import funkcje
-
 engine = db.create_engine("mysql://root:2002Madzia@localhost:3306/snake")
 connection = engine.connect()
 metadata = db.MetaData()
@@ -14,3 +11,17 @@ print("Success:", engine)#udalo sie polaczyc z baza danych
 Session = sessionmaker(bind=engine)
 session = Session()
 funkcje.czytaj_dane_do_logowania(session)
+funkcje.czytaj_informacje(session)
+# tworzenie okna
+
+menu_start = Tk()#tworzenie menu do logowania
+ikona_dla_okienka = PhotoImage(file = "C:/Users/user/PycharmProjects/snake_2/window_icon.png")
+menu_start.resizable(width = False , height= False) # ustawienie braku mozliwosci zmiany rozmiaru okna
+menu_start.geometry("750x750")#wybieranie rozdzielczosci
+menu_start.configure(bg='#76e95f', )#tlo okna ustawione na zielony kolorek
+menu_start.title("SNAKE ")# ustwienie tytułu okienka
+menu_start.iconphoto(False,ikona_dla_okienka)
+
+
+
+menu_start.mainloop()
